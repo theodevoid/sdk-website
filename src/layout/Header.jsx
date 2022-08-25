a52769b384105dbfd29008b98407b3de9716e277
theodevoid <87703322+theodevoid@users.noreply.github.com>
2022-08-25 20:44:05 +0700

    ui: added mobile header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,8 +1,35 @@
+import { Box, IconButton, Icon, Flex, Text } from "@chakra-ui/react"
+import { IoMdMenu } from "react-icons/io"
+
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <Flex
+      padding={{
+        base: "24px 16px",
+        lg: "24px 48px",
+      }}
+      backgroundColor="blackAlpha.900"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Text
+        color="white"
+        textTransform="uppercase"
+        fontFamily="Inter"
+        fontWeight="bold"
+      >
+        Sesuatu Di Kopi
+      </Text>
+      <Icon
+        _hover={{
+          cursor: "pointer",
+        }}
+        color="white"
+        focusable
+        boxSize="8"
+        as={IoMdMenu}
+      />
+    </Flex>
   )
 }
 
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,9 +1,13 @@
 import Header from "layout/Header"
 import Footer from "layout/Footer"
+import Head from "next/head"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>Sesuatu Di Kopi</title>
+      </Head>
       <Header />
       {children}
       <Footer />
