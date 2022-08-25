6864800c15c2ae4c36e6920ce75513b52646952c
theodevoid <87703322+theodevoid@users.noreply.github.com>
2022-08-25 19:54:36 +0700

    chore: setup layout and imports

---
+++ src/layout/index.jsx
@@ -0,0 +1,15 @@
+import Header from "layout/Header"
+import Footer from "layout/Footer"
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
+
