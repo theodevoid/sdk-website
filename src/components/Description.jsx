970f1b1e114dee39e5aa334006eb2a7e1632575d
theodevoid <87703322+theodevoid@users.noreply.github.com>
2022-08-30 20:43:44 +0700

    fix: fixed description alignment and spacing

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,27 +1,26 @@
-const { Box, Text, Flex } = require("@chakra-ui/react")
+const { Box, Text, Flex, Container } = require("@chakra-ui/react")
 
 const Description = () => {
   return (
-    <Flex
-      backgroundColor="blackAlpha.900"
-      color="white"
-      py={{ lg: "14", base: "0" }}
-      pb={{ base: "6" }}
-      pt={{ base: "12" }}
-      direction="row"
-      justifyContent="center"
-      px="4"
-    >
-      <Text
-        maxWidth="410px"
-        textTransform="uppercase"
-        fontSize="3xl"
-        fontWeight="bold"
-        lineHeight="1.25"
-      >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      </Text>
-    </Flex>
+    <Box backgroundColor="blackAlpha.900">
+      <Container maxW="container.lg" py={{ lg: "32", base: "14" }}>
+        <Flex
+          color="white"
+          direction="row"
+          justifyContent={{ base: "center", lg: "start" }}
+        >
+          <Text
+            maxWidth={{ base: "410px", lg: "756px" }}
+            textTransform="uppercase"
+            fontSize="3xl"
+            fontWeight="bold"
+            lineHeight="1.25"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </Text>
+        </Flex>
+      </Container>
+    </Box>
   )
 }
 
